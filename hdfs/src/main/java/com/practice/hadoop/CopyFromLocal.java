f0e3c4a0fa82fc44085213c8cad61180d7c4ba21
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-11-14 02:01:53 +0900

    update source

---
+++ hdfs/src/main/java/com/practice/hadoop/CopyFromLocal.java
@@ -0,0 +1,29 @@
+package com.practice.hadoop;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+public class CopyFromLocal {
+    public static void main(String[] args) throws IOException{
+        String localSrc = args[0];
+        String dst = args[1];
+
+        InputStream in = new BufferedInputStream(new FileInputStream(localSrc));
+        
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(dst), conf);
+        OutputStream out = fs.create(new Path(dst));
+
+        IOUtils.copyBytes(in, out, 4096, true);
+    }
+}
+
