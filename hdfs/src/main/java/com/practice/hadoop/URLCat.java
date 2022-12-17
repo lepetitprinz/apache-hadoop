bbaee34d4f35c6e76c0b68f78627a1e9abf48610
Yongjin Kim <lepetitprinz0911@gmail.com>
2022-12-17 16:33:29 +0900

    Create URLCat.java

---
+++ hdfs/src/main/java/com/practice/hadoop/URLCat.java
@@ -0,0 +1,26 @@
+package com.practice.hadoop;
+
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.InputStream;
+import java.net.URL;
+
+// Reading Data from a Hadoop URL
+public class URLCat {
+
+    static {
+        // this method can be called only once per JVM
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+
+    public static void main(String[] args) throws Exception {
+        InputStream in = null;
+        try {
+            in = new URL(args[0]).openStream();
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(in);
+        }
+    }
+}
